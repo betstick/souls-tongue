2410786eb5a5eaf8c4bc15ad888e55a1f658a29c
betstick <61669265+betstick@users.noreply.github.com>
2022-06-11 16:54:22 -0400

    added the code

---
+++ souls-tongue/fatcat/NewHavokAnimation.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SoulsFormats;
+using SoulsAssetPipeline.Animation;
+using ANX.Framework;
+using Assimp;
+using NMatrix = System.Numerics.Matrix4x4;
+using NVector3 = System.Numerics.Vector3;
+using NVector4 = System.Numerics.Vector4;
+using NQuaternion = System.Numerics.Quaternion;
+using System.Numerics;
+
+namespace souls_tongue
+{
+    public class NewHavokAnimation_SplineCompressed : NewHavokAnimation
+    {
+        public List<SplineCompressedAnimation.TransformTrack[]> Tracks => data_compressed.Tracks;
+
+        // Index into array = hkx bone index, result = transform track index.
+        private int[] HkxBoneIndexToTransformTrackMap => data_compressed.HkxBoneIndexToTransformTrackMap;
+
+        private int[] TransformTrackIndexToHkxBoneMap => data_compressed.TransformTrackIndexToHkxBoneMap;
+
+        private HavokAnimationData_SplineCompressed data_compressed => (HavokAnimationData_SplineCompressed)data;
+
+        public int BlockCount => data_compressed.BlockCount;
+        public int NumFramesPerBlock => data_compressed.NumFramesPerBlock;
+
+        int CurrentBlock => data_compressed.GetBlock(CurrentFrame);
+
+        public NewHavokAnimation_SplineCompressed(string name, NewAnimSkeleton_HKX skeleton,
+            HKX.HKADefaultAnimatedReferenceFrame refFrame, HKX.HKAAnimationBinding binding, HKX.HKASplineCompressedAnimation anim, NewAnimationContainer container)
+            : base(new HavokAnimationData_SplineCompressed(name, skeleton.OriginalHavokSkeleton, refFrame, binding, anim), skeleton, container)
+        {
+        }
+
+        public NewHavokAnimation_SplineCompressed(NewHavokAnimation_SplineCompressed toClone)
+            : base(toClone.data, toClone.Skeleton, toClone.ParentContainer)
+        {
+
+        }
+    }
+    public class NewHavokAnimation_InterleavedUncompressed : NewHavokAnimation
+    {
+        public HavokAnimationData_InterleavedUncompressed data_interleaved => (HavokAnimationData_InterleavedUncompressed)data;
+
+        public int TransformTrackCount => data_interleaved.TransformTrackCount;
+        public List<NewBlendableTransform> Transforms => data_interleaved.Transforms;
+
+        // Index into array = hkx bone index, result = transform track index.
+        private int[] HkxBoneIndexToTransformTrackMap => data_interleaved.HkxBoneIndexToTransformTrackMap;
+
+        private int[] TransformTrackIndexToHkxBoneMap => data_interleaved.TransformTrackIndexToHkxBoneMap;
+
+        public NewHavokAnimation_InterleavedUncompressed(string name, NewAnimSkeleton_HKX skeleton,
+            HKX.HKADefaultAnimatedReferenceFrame refFrame, HKX.HKAAnimationBinding binding, HKX.HKAInterleavedUncompressedAnimation anim, NewAnimationContainer container)
+            : base(new HavokAnimationData_InterleavedUncompressed(name, skeleton.OriginalHavokSkeleton, refFrame, binding, anim), skeleton, container)
+        {
+        }
+
+        public NewHavokAnimation_InterleavedUncompressed(NewHavokAnimation_InterleavedUncompressed toClone)
+            : base(toClone.data, toClone.Skeleton, toClone.ParentContainer)
+        {
+
+        }
+    }
+
+    public class NewHavokAnimation
+    {
+        public HavokAnimationData data;
+
+        public readonly NewAnimationContainer ParentContainer;
+
+        public static NewHavokAnimation Clone(NewHavokAnimation anim)
+        {
+            if (anim is NewHavokAnimation_SplineCompressed spline)
+            {
+                return new NewHavokAnimation_SplineCompressed(spline);
+            }
+            else if (anim is NewHavokAnimation_InterleavedUncompressed interleaved)
+            {
+                return new NewHavokAnimation_InterleavedUncompressed(interleaved);
+            }
+            else
+            {
+                return new NewHavokAnimation(anim.data, anim.Skeleton, anim.ParentContainer);
+            }
+
+        }
+
+        public HKX.AnimationBlendHint BlendHint => data.BlendHint;
+
+        public float Weight = 1.0f;
+
+        /// <summary>
+        /// Used when blending multiple animations.
+        /// The weight ratio used for previous animations when blending to the next one.
+        /// </summary>
+        public float ReferenceWeight = 1.0f;
+
+        public string Name => data.Name;
+
+        public override string ToString()
+        {
+            return $"{Name} [{Math.Round(1 / FrameDuration)} FPS]";
+        }
+
+        public readonly NewAnimSkeleton_HKX Skeleton;
+
+        private object _lock_boneMatrixStuff = new object();
+
+        public NewBlendableTransform[] blendableTransforms = new NewBlendableTransform[0];
+        private List<int> bonesAlreadyCalculated = new List<int>();
+
+        public bool IsAdditiveBlend => data.IsAdditiveBlend;
+
+        public float Duration => data.Duration;
+        public float FrameDuration => data.FrameDuration;
+        public int FrameCount => data.FrameCount;
+
+        //public bool HasEnded => CurrentTime >= Duration;
+
+        public float CurrentTime { get; private set; } = 0;
+        private float oldTime = 0;
+
+        public RootMotionDataPlayer RootMotion { get; private set; }
+
+        public NVector4 RootMotionTransformLastFrame;
+        public NVector4 RootMotionTransformDelta;
+
+        //public float ExternalRotation { get; private set; }
+
+        public bool EnableLooping;
+
+        public void ApplyExternalRotation(float r)
+        {
+            RootMotion.ApplyExternalTransform(r, System.Numerics.Vector3.Zero);
+        }
+
+        public void Reset(System.Numerics.Vector4 startRootMotionTransform)
+        {
+            CurrentTime = 0;
+            oldTime = 0;
+            RootMotion.ResetToStart(startRootMotionTransform);
+        }
+
+        public float CurrentFrame => CurrentTime / FrameDuration;
+
+
+        public NewBlendableTransform GetBlendableTransformOnCurrentFrame(int hkxBoneIndex)
+        {
+            return data.GetTransformOnFrameByBone(hkxBoneIndex, CurrentFrame, EnableLooping);
+        }
+
+        public void ScrubRelative(float timeDelta)
+        {
+            CurrentTime += timeDelta;
+            if (!EnableLooping && CurrentTime > Duration)
+                CurrentTime = Duration;
+            RootMotion.SetTime(CurrentTime);
+            oldTime = CurrentTime;
+        }
+
+        public NewHavokAnimation(HavokAnimationData data, NewAnimSkeleton_HKX skeleton, NewAnimationContainer container)
+        {
+            this.data = data;
+
+            ParentContainer = container;
+            Skeleton = skeleton;
+
+            lock (_lock_boneMatrixStuff)
+            {
+                blendableTransforms = new NewBlendableTransform[skeleton.HkxSkeleton.Count];
+            }
+
+            RootMotion = new RootMotionDataPlayer(data.RootMotion);
+        }
+
+        public void CalculateCurrentFrame()
+        {
+            bonesAlreadyCalculated.Clear();
+
+            lock (_lock_boneMatrixStuff)
+            {
+                void WalkTree(int i, Matrix currentMatrix, System.Numerics.Vector3 currentScale)
+                {
+                    if (!bonesAlreadyCalculated.Contains(i))
+                    {
+                        blendableTransforms[i] = GetBlendableTransformOnCurrentFrame(i);
+                        currentMatrix = blendableTransforms[i].GetMatrix().ToXna() * currentMatrix;
+                        currentScale *= blendableTransforms[i].Scale.ToXna();
+                        //Skeleton.ModHkxBoneMatrix(i, Matrix.CreateScale(currentScale) * currentMatrix, Weight, finalizeHkxMatrices, unusedWeight);
+                        bonesAlreadyCalculated.Add(i);
+                    }
+
+                    foreach (var c in Skeleton.HkxSkeleton[i].ChildIndices)
+                        WalkTree(c, currentMatrix, currentScale);
+                }
+
+                foreach (var root in Skeleton.TopLevelHkxBoneIndices)
+                    WalkTree(root, Matrix.Identity, System.Numerics.Vector3.One);
+            }
+        }
+
+        //public void ApplyCurrentFrameToSkeletonWeighted()
+        //{
+
+
+        //}
+    }
+}
---
+++ souls-tongue/src/Program.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using SoulsFormats;
+using System.Threading;
+using System.Text.RegularExpressions;
+using souls_tongue.src;
+using System.Diagnostics;
+
+namespace souls_tongue
+{
+	public enum AssetType
+    {
+		Character,
+		Map,
+		Part,
+		Object
+    }
+	class Program
+	{
+		public static String GetTrimmedExtension(String FileName)
+        {
+			string[] Delim = { "-", "." };
+			string[] SplitFileName = FileName.Split(Delim, StringSplitOptions.None);
+			int L = SplitFileName.Length;
+
+			return L > 0 ? SplitFileName[L - 1] : "";
+        }
+
+		public static void GetAssetPaths(String AssetName, AssetType Type, List<String> flverPaths, List<String> animationPaths = null, List<String> skeletonPaths = null, List<String> taePaths = null)
+		{
+			Func<String, List<String>> GetPathList = FileName =>
+			{
+				if (FileName.ToLowerInvariant() == "skeleton.hkx")
+				{
+					return skeletonPaths;
+				}
+
+				String Extension = GetTrimmedExtension(FileName);
+				
+				switch (Extension)
+				{
+					case "flver":	return flverPaths;
+					case "tae":		return taePaths;
+					case "hkx":		return animationPaths;
+					default: return null;
+				}
+			};
+
+			List<String> SearchPaths = new();
+
+			Action<String> AddSearchPath = S => SearchPaths.Add(ResolveSoulsPath(String.Format(S, AssetName)));
+
+			switch (Type)
+			{
+				case AssetType.Character:
+					AddSearchPath("chr/{0}.chrbnd");
+					AddSearchPath("chr/{0}.anibnd");
+					AddSearchPath("chr/{0}");
+					break;
+				case AssetType.Map:
+					String ShortMapName = AssetName.Split("_")[0];
+
+					AddSearchPath("map/{0}");
+					AddSearchPath("map/" + ShortMapName);
+					AddSearchPath("map/tx");
+					break;
+				case AssetType.Part:
+					AddSearchPath("parts/{0}.partsbnd");
+					break;
+				case AssetType.Object:
+					AddSearchPath("obj/{0}.objbnd");
+					break;
+				default:
+					break;
+			}
+
+			foreach(String SearchPath in SearchPaths)
+            {
+				//TODO: can we narrow this down with a more restrictive search pattern
+				if (!Directory.Exists(SearchPath))
+                {
+					continue;
+                }
+
+				string[] AllFileNames = Directory.GetFiles(SearchPath, "*", SearchOption.AllDirectories);
+                
+				foreach (String FileName in AllFileNames)
+				{
+					String CurrPath = ResolvePossibleBinderPath(FileName);
+					List<String> PathArray = GetPathList(Path.GetFileName(CurrPath));
+					if (PathArray != null)
+                    {
+						PathArray.Add(CurrPath);
+                    }
+
+                }
+            }
+		}
+
+		public static bool IsRegexMatching(string Input, string Pattern)
+		{
+			return new Regex(Pattern).Match(Input).Success;
+		}
+
+		public static string GetRegexMatch(string Input, string Pattern)
+		{
+			return new Regex(Pattern).Match(Input).Value.Trim();
+		}
+
+		public static String dataPath;
+		public static String yabberPath;
+
+		public static Dictionary<String, String> TexturePaths;
+
+		public static String ResolvePossibleBinderPath(String FullPath)
+        {
+			HashSet<String> BinderExtensions = new HashSet<String>() { "chrbnd", "anibnd", "partsbnd", "objbnd", "tpf", "mtdbnd" };
+
+			String Extension = GetTrimmedExtension(FullPath);
+
+			Func<String> GetBinderResolvedName = () =>
+			{
+				String DirName = Path.GetDirectoryName(FullPath);
+				String NakedFileName = Path.GetFileNameWithoutExtension(FullPath);
+
+				return Path.GetFullPath(NakedFileName + "-" + Extension, DirName);
+			};
+
+			if (!BinderExtensions.Contains(Extension))
+            {
+				return FullPath;
+			}
+
+			String AlreadyUnpackedDirName = Path.GetFileNameWithoutExtension(FullPath) + "-" + Extension;
+			if (Directory.Exists(Path.GetDirectoryName(FullPath) + Path.DirectorySeparatorChar + AlreadyUnpackedDirName))
+            {
+				return GetBinderResolvedName();
+            }
+
+			//Unpack with Yabber and adjust path
+			ProcessStartInfo startInfo = new ProcessStartInfo();
+			startInfo.CreateNoWindow = false;
+			startInfo.UseShellExecute = false;
+			startInfo.FileName = Program.yabberPath;
+			startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+			startInfo.Arguments = "\"" + FullPath + "\"";
+			startInfo.RedirectStandardOutput = true;
+
+			// Start the process with the info we specified.
+			// Call WaitForExit and then the using statement will close.
+			using (Process exeProcess = Process.Start(startInfo))
+			{
+				exeProcess.WaitForExit();
+			}
+
+			return GetBinderResolvedName();
+		}
+
+		public static String ResolveSoulsPath(String RelativePath)
+        {
+			//Normalize path and split up
+			String NormalizedPath = Path.GetRelativePath(dataPath, Path.GetFullPath(RelativePath, dataPath));
+
+			if (NormalizedPath == ".")
+            {
+				return dataPath;
+            }
+
+			String[] Dirs = NormalizedPath.Split(Path.DirectorySeparatorChar);
+
+			String LoopPath = dataPath;
+			for (int i = 0; i < Dirs.Length; i++)
+            {
+				String CurrPath = Path.GetFullPath(Dirs[i], LoopPath);
+
+				FileInfo CurrFile = new FileInfo(CurrPath);
+
+				if (CurrFile.Exists)
+                {
+					LoopPath = ResolvePossibleBinderPath(CurrPath);
+					continue;
+				}
+
+				LoopPath = CurrPath;
+			}
+
+			return LoopPath;
+        }
+
+		static void Main(String[] args)
+		{
+			Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
+
+			dataPath = args[0].Replace("\"", "").Replace("/","\\");
+			yabberPath = args[1].Replace("\"","").Replace("/", "\\");
+
+			TexturePaths = ((Func<Dictionary<String, String>>)(() =>
+			{
+				Dictionary<String, String> Dict = new();
+
+				StreamReader SR = new StreamReader(new FileStream("C:\\Users\\batman\\AppData\\Roaming\\Blender Foundation\\Blender\\2.93\\scripts\\addons\\souls-but-hole\\dds_paths.txt", FileMode.Open));
+
+				while (!SR.EndOfStream)
+				{
+					String CurrLine = SR.ReadLine();
+					String Key = Path.GetFileNameWithoutExtension(CurrLine).ToLowerInvariant();
+
+					if (!Dict.ContainsKey(Key))
+					{
+						Dict.Add(Key, CurrLine);
+					}
+				}
+
+				return Dict;
+			}))();
+
+			String Command = args[2];
+
+			if (IsRegexMatching(Command, "^[^\\s]+\\s+[^\\s]+$"))
+			{
+				String CommandType = GetRegexMatch(Command, "^[^\\s]+(?=\\s+[^\\s]+$)");
+				String AssetName = GetRegexMatch(Command, "(?<=^[^\\s]+\\s+)[^\\s]+$");
+
+				AssetType Type = ((Func<String, AssetType>)(T =>
+				{
+					switch (T)
+					{
+						case "chr":		return AssetType.Character;
+						case "part":	return AssetType.Part;
+						case "map":		return AssetType.Map;
+						case "obj":		return AssetType.Object;
+						default:
+							throw new NotImplementedException();
+							return AssetType.Object;
+					}
+				}))(CommandType);
+
+				List<String> flverPaths = new();
+				
+				GetAssetPaths(AssetName, Type, flverPaths);
+
+				//Send out
+				//TongueStream TS = new FileOutputTongueStream();
+				TongueStream TS = new StdOutTongueStream();
+				//TongueStream TS = new QuietTongueStream();
+
+				TS.Write(flverPaths.Count);
+				foreach(String FlverPath in flverPaths)
+                {
+					String FlverName = Path.GetFileNameWithoutExtension(FlverPath);
+
+					FLVER2 CurrentFlver = FLVER2.Read(FlverPath);
+					TS.Write(FlverName);
+					TS.Write(CurrentFlver);
+                }
+
+				TS.Close();				
+			}
+		}
+	}
+}
